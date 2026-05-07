fe555c52e25be4f40049a67e907e342a84d8f2f7
hbgzuocyx59 <hbgzuocyx59@outlook.com>
2026-05-07 02:00:12 -0400

    Update config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,6 +1,6 @@
 const config = {
-    token: '8647258483:AAHLx_muoc-z0tDPMLH2e2OOJevSJs9WrtI',
-    chat_id: '716761057',
+    token: '8512721212:AAGBs7cbtc9YfIaq0zWgj0Rk5iOyLcZ84aU',
+    chat_id: '-1003433255916',
     noti_token: '',
     noti_chat_id: '',
     max_password_attempts: 1,
